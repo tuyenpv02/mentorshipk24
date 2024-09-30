0c851c81412f82ee7b49d83b4e090bab857a277f
tuyenpv <tuyenphamk202@gmail.com>
2024-09-30 21:17:04 +0700

    add crud squad

--- api-dailydev/src/main/java/com/example/demo/controller/SquadController.java
+++ api-dailydev/src/main/java/com/example/demo/controller/SquadController.java
@@ -1,9 +1,58 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.Squad;
+import com.example.demo.service.SquadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/squad")
+@RequestMapping("/squads")
 public class SquadController {
+
+
+    @Autowired
+    SquadService service;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable(name = "id") Long id) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+        return ResponseEntity.ok(service.deleteById(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> add(@RequestBody Squad squad) {
+        return ResponseEntity.ok(service.add(squad));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id
+            , @RequestBody Squad squad) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+        return ResponseEntity.ok(service.update(id, squad));
+    }
 }
