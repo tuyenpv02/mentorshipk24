df01193ad2e968c09fba3324333fe54a10447ff0
tuyen <tuyenphamk202@gmail.com>
2024-10-01 20:00:57 +0700

    add bookmark

--- api-dailydev/src/main/java/com/example/demo/repository/VoteRepository.java
+++ api-dailydev/src/main/java/com/example/demo/repository/VoteRepository.java
@@ -4,6 +4,13 @@ import com.example.demo.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    List<Vote> findAllByUserId(Long userId);
+    List<Vote> findAllByPostId(Long postId);
+
+
 }
