50f853b9798a25b0d118ad1e5162c8b898530c24
tuyen <tuyenphamk202@gmail.com>
2024-10-09 22:48:52 +0700

    update api reaction

---
+++ api-dailydev/src/main/java/com/example/demo/controller/VoteController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Vote;
+import com.example.demo.service.VoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/vote")
+public class VoteController {
+
+    @Autowired
+    VoteService service;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable(name = "id") Long id) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+        return ResponseEntity.ok(service.deleteById(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> add(@RequestBody Vote vote) {
+        return ResponseEntity.ok(service.add(vote));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id
+            , @RequestBody Vote vote) {
+        if (!service.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    "Không tìm thấy"
+            );
+        }
+
+        return ResponseEntity.ok(service.update(id, vote));
+    }
+}
